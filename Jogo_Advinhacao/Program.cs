0c1fbc49b923c9fc552d6f0d38d662ec0075798b
Matheus Henrique da Silva <matheeus.silva9@hotmail.com>
2024-05-17 16:35:27 -0300

    Jogo da advinhação, semi-pronto

---
+++ Jogo_Advinhacao/Program.cs
@@ -0,0 +1,36 @@
+﻿int numeroAleatorio = new Random().Next(1,101);
+int Tentativas = 0;
+int chute = 0;
+
+Console.WriteLine(@"
+██╗░░░░░███████╗██╗░█████╗░  ███╗░░░███╗██╗███╗░░██╗██╗░░██╗░█████╗░
+██║░░░░░██╔════╝██║██╔══██╗  ████╗░████║██║████╗░██║██║░░██║██╔══██╗
+██║░░░░░█████╗░░██║███████║  ██╔████╔██║██║██╔██╗██║███████║███████║
+██║░░░░░██╔══╝░░██║██╔══██║  ██║╚██╔╝██║██║██║╚████║██╔══██║██╔══██║
+███████╗███████╗██║██║░░██║  ██║░╚═╝░██║██║██║░╚███║██║░░██║██║░░██║
+╚══════╝╚══════╝╚═╝╚═╝░░╚═╝  ╚═╝░░░░░╚═╝╚═╝╚═╝░░╚══╝╚═╝░░╚═╝╚═╝░░╚═╝
+
+███╗░░░███╗███████╗███╗░░██╗████████╗███████╗
+████╗░████║██╔════╝████╗░██║╚══██╔══╝██╔════╝
+██╔████╔██║█████╗░░██╔██╗██║░░░██║░░░█████╗░░
+██║╚██╔╝██║██╔══╝░░██║╚████║░░░██║░░░██╔══╝░░
+██║░╚═╝░██║███████╗██║░╚███║░░░██║░░░███████╗
+╚═╝░░░░░╚═╝╚══════╝╚═╝░░╚══╝░░░╚═╝░░░╚══════╝");
+Console.WriteLine("\nBem vindo ao jogo de adivinhação! ");
+Console.WriteLine("-----------------------------------");
+
+while (chute != numeroAleatorio) 
+{
+    Console.Write("Adivinhe o número que estou pensando de 1 até 100: ");
+    chute = int.Parse(Console.ReadLine()!);
+    if (chute > numeroAleatorio){
+        Console.WriteLine("Errou, o número que estou pensando é menor!\n ");
+        Tentativas++;    
+    }
+    else if (chute < numeroAleatorio){
+        Console.WriteLine("Errou, o número que estou pensando é maior!\n ");
+        Tentativas++;
+    } 
+}
+Tentativas++;
+Console.WriteLine("Parabéns você acertou!!! com " + Tentativas + " Tentativas");
